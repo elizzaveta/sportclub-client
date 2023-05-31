dea2f4b650975889ecc9182c3dada5bcc0bb9a9e
Yelizaveta <yelizaveta.timchenko21@gmail.com>
2023-06-01 00:31:50 +0300

    Updated qr code scanner component

--- app/admin/manage-client/qr-code-scanner/page.tsx
+++ app/admin/manage-client/qr-code-scanner/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import React, {useEffect} from 'react';
 import {useRouter} from "next/navigation";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+// import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import {qrCodeDataType} from "@/types/qrCodeDataType";
+import { QrReader } from 'react-qr-reader';
 
 export default function QrCodeScannerPage() {
     const [qrCodeData, setQrCodeData] = React.useState<qrCodeDataType>();
@@ -18,12 +19,13 @@ export default function QrCodeScannerPage() {
     return (
         <div className='flex flex-col items-center'>
             <p className='text-3xl font-bold text-sky-600'>QR Code Scanner</p>
-            <BarcodeScannerComponent
-                width={500}
-                height={500}
-                onUpdate={(err, result) => {
+            <QrReader
+                scanDelay={500}
+                onResult={(result, error) => {
                     if (result) setQrCodeData(JSON.parse(result.getText()) as qrCodeDataType);
                 }}
+                className='w-96 h-96'
+                constraints={{ facingMode: 'user' }}
             />
             {/*<p>{JSON.stringify(qrCodeData)}</p>*/}
         </div>
